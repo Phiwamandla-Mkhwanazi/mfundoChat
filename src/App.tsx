dac041cae0cbf464cd0dd26185e661783b117ce6
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-06-01 19:25:07 +0200

    Chat App basic Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen text-green-900 bg-black'>
+      mfundoChat Web Application
+    </div>
+  )
+}
+
+export default App
